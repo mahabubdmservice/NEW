86f0bab291b4fc3d0c0369b1a4659bc5e6f4c42e
mahabubdmservice <mahabuburrahman.jsr@gmail.com>
2022-09-08 16:03:24 +0600

    new key word with using class dstructor

---
+++ new.js
@@ -0,0 +1,17 @@
+
+// এখানে নিউ কি-ওয়ার্ড ব্যবহার কোন একটি ক্লাস থেকে অনেক অবজেক্ট তৈরী করা হলো-
+// Attribute, Property, Key কাছাকাছি টাইপের জিনিস-
+// Making a class for making many object :- 
+// similar in pattern or similar object thakla class a korta hoy
+// New কি করে ? - New ক্লাস থেকে একটি নতুন অবজেক্ট তৈরী করে---
+class Person {
+    constructor(x, y, salary){
+        this.firstName = x;
+        this.lastName = y;
+        this.salary = this.salary;
+    }
+}
+const heroPerson = new Person("Mizan", "Zahin", 2000);
+console.log(heroPerson);
+const friendlyPerson = new Person("Mahabub", "Hassan", 23608);
+console.log(friendlyPerson);
